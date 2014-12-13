241fd8eebe9fce73f1220a31f2534afb43fd860e
maxwen <max.weninger@gmail.com>
2014-12-13 22:46:01 +0100

    Base: add flashlight toggle
    modified by @Evisceration
    send broadcast Intent.ACTION_TOGGLE_FLASHLIGHT to toggle the flashlight
    
    Signed-off-by: Evisceration <alexander.martinz@nameless-rom.org>
    Change-Id: I0601bbfb05b3b28b2bedde2602b26e3ec61c27e7
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1183,6 +1183,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_TOGGLE_FLASHLIGHT);
         if (DEBUG_MEDIA_FAKE_ARTWORK) {
             filter.addAction("fake_artwork");
         }
@@ -3575,6 +3576,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 if (DEBUG_MEDIA_FAKE_ARTWORK) {
                     updateMediaMetaData(true);
                 }
+            } else if (Intent.ACTION_TOGGLE_FLASHLIGHT.equals(action)) {
+                mFlashlightController.toggleFlashlight();
             }
         }
     };
