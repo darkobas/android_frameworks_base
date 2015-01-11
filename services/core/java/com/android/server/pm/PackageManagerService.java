a423e539def2bce66eebc79669b7757fe616eb37
Paul Bouman <boumanpaul@gmail.com>
2015-01-11 13:54:27 +0100

    Show Optimizing app dialog on first boot
    The boot dialog is currently hidden on first boot but the bootanimation
    can take a very long time to go away. Show the boot dialog so we know
    whats happening.
    
    Change-Id: I71d50eb8929edde38fcfdcccc2cb05c25777c6dd
    
    Conflicts:
    	services/core/java/com/android/server/pm/PackageManagerService.java

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4775,23 +4775,14 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
-        if (!isFirstBoot()) {
-            try {
-                // give the packagename to the PhoneWindowManager
-                ApplicationInfo ai;
-                try {
-                    ai = mContext.getPackageManager().getApplicationInfo(pkg.packageName, 0);
-                } catch (Exception e) {
-                    ai = null;
-                }
-                mPolicy.setPackageName((String) (ai != null ? mContext.getPackageManager().getApplicationLabel(ai) : pkg.packageName));
-
-                ActivityManagerNative.getDefault().showBootMessage(
-                        mContext.getResources().getString(R.string.android_upgrading_apk,
-                                curr, total), true);
-            } catch (RemoteException e) {
-            }
+        final String bootMsg = mContext.getResources()
+                .getString(R.string.android_upgrading_apk, curr, total)
+                + "\n(" + pkg.packageName + ')';
+        try {
+            ActivityManagerNative.getDefault().showBootMessage(bootMsg, true);
+        } catch (RemoteException e) {
         }
+
         PackageParser.Package p = pkg;
         synchronized (mInstallLock) {
             performDexOptLI(p, null /* instruction sets */, false /* force dex */,
