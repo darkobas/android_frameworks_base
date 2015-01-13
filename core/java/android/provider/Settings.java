6f7c43bd33d366850589cce3129759d22351cae7
darkobas <darkobas@gmail.com>
2015-01-13 23:05:56 +0100

    Revert "Frameworks: Slim heads up customizations for LP (1/2)"
    This reverts commit e4ba9c227f14972f85b397e1aad857ae4f323232.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	packages/SystemUI/res/layout/notification_guts.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
    	services/core/java/com/android/server/am/ActivityStack.java
    	services/core/java/com/android/server/statusbar/StatusBarManagerService.java
    
    Change-Id: I11c2529a94958b57669e9ca12a6034d67ac9970a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2095,20 +2095,6 @@ public final class Settings {
         public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
 
         /**
-         * Defines global heads up snooze time if user want to snooze it.
-         *
-         * @hide
-         */
-        public static final String HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
-
-        /**
-         * Defines the global heads up notification decay
-         *
-         * @hide
-         */
-        public static final String HEADS_UP_NOTIFCATION_DECAY = "heads_up_notification_decay";
-
-        /**
          * Defines the status and navigation bar color is battery safe mode is enabled.
          *
          * @hide
